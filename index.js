b418babf695d6ef56e6e65cde5fafb69a2b3069c
Simran Kaur <simrank.adept@gmail.com>
2022-12-20 23:59:12 -0800

    chore(hook): changed to before artifact creation

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class ServerlessPlugin {
 
     this.hooks = {
       //'after:aws:deploy:deploy:updateStack': this.updateVersionToSsm.bind(this)
-      'after:deploy:deploy': () => BbPromise.bind(this)
+      'before:package:createDeploymentArtifacts': () => BbPromise.bind(this)
                 .then(this.updateVersionToSsm)
     };
   }
